6bcb8484fe37ae6299700663de3061d390dee83e
Ed Mason <mynameisprivate3@gmail.com>
2021-02-13 13:23:46 +0000

    Trying to put w3w coords on Map (and failing)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import App from "next/app";
 import "tailwindcss/tailwind.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export default App;
--- pages/b/[slug].tsx
+++ pages/b/[slug].tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Head from "next/head";
 import Nav from "../../components/nav";
+import ReactMapGL from "react-map-gl";
 
 const Bench = ({ bench }) => {
+  const api = require("@what3words/api");
+  api.setOptions({ key: "ZHCEP0DY" });
+  const coords = api.convertToCoordinates(bench.location);
+
+  const [viewport, setViewport] = useState({
+    width: 1000,
+    height: 400,
+    latitude: 53,
+    longitude: 0,
+    zoom: 16,
+  });
+
   return (
     <div>
       <Head>
@@ -18,10 +31,16 @@ const Bench = ({ bench }) => {
         <h2>{bench.condition}</h2>
         <br />
         <h5>{bench.capacity}</h5>
-        <br/>
+        <br />
         <p>{bench.description}</p>
         <br />
-        <p>{bench.location}</p>
+        <div className="content-center">
+          <ReactMapGL
+            mapStyle="mapbox://styles/mapbox/outdoors-v11"
+            mapboxApiAccessToken="pk.eyJ1IjoiZWRhcG0iLCJhIjoiY2tsM29kOWtzMTBvdzMwdDd2b3dtNHYxNiJ9.ZGBau9yxktkf-2G6p57eig"
+            {...viewport}
+          ></ReactMapGL>
+        </div>
       </div>
     </div>
   );
