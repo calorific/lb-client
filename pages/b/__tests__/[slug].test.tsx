f823d3f9a0b848e1f7615e28525c514d3d923098
Ed Mason <mynameisprivate3@gmail.com>
2021-02-20 15:47:03 +0000

    Finish Jest config, add test files, add LICENSE

---
+++ pages/b/__tests__/[slug].test.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import Bench from "../[slug]";
+
+test("Bench", () => {});
---
+++ pages/b/__tests__/index.test.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import BenchesIndex from "../index";
+
+test("BenchesIndex", () => {});
